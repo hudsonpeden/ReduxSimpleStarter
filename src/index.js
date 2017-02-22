cc09ee5cfb81eafd891a9a7ff4d41c322c31455b
Hudson Peden <hpeden@logicalsysinc.com>
2017-02-22 16:42:00 -0600

    finished section 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBwgKt3HJy7fYT9QpxFt1wD07OJuzWMhVU';
+
 // Create new component that should produce some HTML.
 
-const App = function () {
-  return <div>Hi</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+};
 
 
 // Take this compnent's HTML and put it on the page.
